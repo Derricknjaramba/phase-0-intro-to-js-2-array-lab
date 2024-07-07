fdfb519112afb1f4da0ada7048c90d5f39f77856
Derricknjaramba <njarambaderrick@gmail.com>
2024-07-07 13:32:00 +0300

    js2array

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution here!
+
+let cats = ["Milo", "Otis", "Garfield"];
+let destructivelyAppendCat=function(){
+    cats.push("Ralph");
+    console.log(cats);
+}
+let destructivelyPrependCat=function(){
+    cats.unshift("Bob");
+    console.log(cats);
+}
+    let destructivelyRemoveLastCat=function(){
+        cats.pop();
+        console.log(cats);
+    }
+    let destructivelyRemoveFirstCat=function(){
+        cats.shift();
+        console.log(cats);
+    }
+    function appendCat(name){
+        return[...cats,name];
+    }
+    function prependCat(name){
+        return[name, ...cats];
+
+    }
+    function removeLastCat(){
+        return cats.slice(0,cats.length -1);
+    }
+    function removeFirstCat(){
+        return cats.slice(1,cats.length);
+    }
+
+
